7dea5f37355c55283ca8ca919e89e281e6321a7f
Mariana Huh <mhuh0176@gmail.com>
2019-08-02 19:08:12 -0700

    project

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -2,4 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
+const ProjectDB = require('../data/helpers/projectModel');
+
+router.use((req, res, next) => {
+  console.log('>>>>>>  FROM_PROJECT_ROUTER  <<<<<<');
+  next();
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const Projects = await ProjectDB.get();
+    res.status(200).json(Projects);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ err: err.message, message: 'Error retrieving projects!' });
+  }
+});
+
 module.exports = router;

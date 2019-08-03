5e3734cbf6fc5f6d10a70ada55ca1ab344b87dc7
Mariana Huh <mhuh0176@gmail.com>
2019-08-02 21:16:25 -0700

    submitting mvp

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 const ActionsDb = require('../data/helpers/actionModel');
-const ProjectsDB = require('../data/helpers/projectModel');
 
 router.use((req, res, next) => {
   console.log('>>>>>>  FROM_ACTION_ROUTER  <<<<<<');
@@ -27,6 +26,52 @@ router.get('/:id', async (req, res) => {
     res
       .status(500)
       .json({ err: err.message, message: 'Error retrieving action!' });
+  }
+});
+
+router.post('/', (req, res) => {
+  const Actions = req.body;
+  ActionsDb.insert(Actions)
+    .then(Actions => {
+      res.status(201).json(Actions);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err.message,
+        message: 'Error getting actions!'
+      });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  ActionsDb.update(id, changes)
+    .then(updated => {
+      if (updated) {
+        res.status(200).json(updated);
+      } else {
+        res.status(400).json({
+          message: 'Please provide name and description for the action.'
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err,
+        message: 'The action information could not be modified.'
+      });
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const Actions = await ActionsDb.remove(req.params.id);
+    res.status(204).json(Actions);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ err: err.message, message: 'Error deleting action' });
   }
 });
 
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -11,12 +11,80 @@ router.use((req, res, next) => {
 
 router.get('/', async (req, res) => {
   try {
-    const Projects = await ProjectDB.get();
-    res.status(200).json(Projects);
+    const project = await ProjectDB.get();
+    res.status(200).json(project);
   } catch (err) {
     res
       .status(500)
       .json({ err: err.message, message: 'Error retrieving projects!' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const project = await ProjectDB.get(req.params.id);
+    res.status(200).json(project);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ err: err.message, message: 'Error retrieving projects!' });
+  }
+});
+
+router.post('/', (req, res) => {
+  const project = req.body;
+  ProjectDB.insert(project)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err.message,
+        message: 'Error retrieving project'
+      });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  ProjectDB.update(id, changes)
+    .then(updated => {
+      if (updated) {
+        res.status(200).json(updated);
+      } else {
+        res.status(400).json({
+          message: 'Please provide name and description for the project.'
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err.message,
+        message: 'Project could not be modified.'
+      });
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const project = await ProjectDB.remove(req.params.id);
+    res.status(204).json(project);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ err: err.message, message: 'Error deleting project!' });
+  }
+});
+
+router.get('/:id/actions', async (req, res) => {
+  try {
+    const project = await ProjectDB.getProjectActions(req.params.id);
+    res.status(200).json(project);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ err: err.message, message: 'Error retrieving actions!' });
   }
 });
 
